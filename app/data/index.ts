43e9271db742a7f81cf6faf6fb80b25efd6c52a0
nirajsathe80 <nirajsathe80@gmail.com>
2024-11-06 13:38:41 +0530

    common data file add

---
+++ app/data/index.ts
@@ -0,0 +1,185 @@
+export const todayEditionArticleArray = [
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/460b/7ba2/cef7f3b55ea636ab34295402fb4c9c30?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a41EfstAmc9klHyhl1MA9SX0rPfpMnXG7cXyqEXy00KTKcpj5oT1b1-Wri-3wQEC8oH1nVpG7xhEiWwfn1YQ5IMURA6QGna5yb-SzJ1jfKGMxINgFq~riFmaolWVICAvERDK~PxSNrvG0rLXIwNDUpg4YqTQdNj4QjWdZHbHM9KoFXxGEyFkfoMbcZ9En4wMudeHR0gH6bMqo020BH19fiI~Ix9ngcyix6HJZqtuwhAHtTG9BPdiuzNdbI2qBVfcGgBvR~53oLWYk43zqHt3qCHYnPlz2IokkLenDtkDBZG54AJ2Y4ezLOKPDHQaun5sF-Ml7X9-xRxiD59SMiXDxg__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman
+          Ajay Singh may have created a toxic work atmosphere and poor safety
+          culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/dc14/c168/a437cbcac0ff201ec24f2c85a9f04d55?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cJ-l9FeoZtPdqqlpvK5SsaMJsk1WPCLpub8cT4yK1liHelP~KwomB0-MAvWYU-Fy7EE08eXCBMery6odqRrp8YLwp2JcBgqufCUjAT~o8a0EHuQw~-pgslExZQ7qYsSZxoT5Dl74zPNusS1rVzmrgChxeN9mrByYxtHhttilyxre7AQOSSLwJMb26fnDmwqAY7npWkNogIqds-bIHQdPJ8fM22En-0aYJ1vIpGihzQIAKHFMWeLZ55ml~v5QvrBVZcrPKGmYLyBHtSfMB-d2NAZdlccL0bl~uKKPyvLgg2zJvpQsocMn3Am3gDiXitrY~OO7POSpBcLLJ6v1ilvJGA__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman
+          Ajay Singh may have created a toxic work atmosphere and poor safety
+          culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/2f8d/35e9/ae7eee1b1d07e829b629c397cb4d2849?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Xns~1vEH-MT9bxydnTJLzvp8cdmXs1Fgh6UdJknvbg-bQrIAdjpetVG~aYXiRZMnVg8150fmRnM4Q~5EqK7hJa19j3TbZiLT3DgBEUKM6FzS1yJSrVnuiuhR3Ne-Z7619gEoaQq2ngo22u4LZ0AXI7bdrjY8gMEaHijyGNDCgpkz23Fx3fIcRx0pGQ67nX9IjiKI~GUfVvcuVhgMM5t6viROUXgjZJATyAnaC~j~l77CtzzbWKNfMsaSIpda7D1tkU5VbPbp-OmXgpmTmCjyW~tM1~qjJHkKj2WcKjKTdA33xZkfQtbeetOHx~KmatMhd8p6OYJ-n-qTAtpDPjq7Ng__",
+    title: "Is Bandhan banking too much on microfinance?",
+    description: `  In trying to keep costs in check at his loss-making airline, chairman
+          Ajay Singh may have created a toxic work atmosphere and poor safety
+          culture.`,
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    articleReadTime: 5,
+    commentsCount: 23,
+  },
+];
+
+export const logos = [
+  {
+    src: "facebook.svg",
+  },
+  {
+    src: "twitter.svg",
+  },
+  {
+    src: "linkedin.svg",
+  },
+  {
+    src: "instagram.svg",
+  },
+  {
+    src: "youtube.svg",
+  },
+];
+
+export const briefNews = [
+  {
+    date: "04 November 2024",
+    title:
+      "Russian Navy's warship is either floating abandoned or at the bottom of the Black Sea",
+    isBreaking: false,
+  },
+  {
+    date: "03 November 2024",
+    title:
+      "Russian Navy's warship is either floating abandoned or at the bottom of the Black Sea",
+    isBreaking: true,
+  },
+  {
+    date: "02 November 2024",
+    title:
+      "Russian Navy's warship is either floating abandoned or at the bottom of the Black Sea",
+    isBreaking: false,
+  },
+  {
+    date: "02 November 2024",
+    title:
+      "Russian Navy's warship is either floating abandoned or at the bottom of the Black Sea",
+    isBreaking: false,
+  },
+];
+
+export const topStoriesArray = [
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/460b/7ba2/cef7f3b55ea636ab34295402fb4c9c30?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a41EfstAmc9klHyhl1MA9SX0rPfpMnXG7cXyqEXy00KTKcpj5oT1b1-Wri-3wQEC8oH1nVpG7xhEiWwfn1YQ5IMURA6QGna5yb-SzJ1jfKGMxINgFq~riFmaolWVICAvERDK~PxSNrvG0rLXIwNDUpg4YqTQdNj4QjWdZHbHM9KoFXxGEyFkfoMbcZ9En4wMudeHR0gH6bMqo020BH19fiI~Ix9ngcyix6HJZqtuwhAHtTG9BPdiuzNdbI2qBVfcGgBvR~53oLWYk43zqHt3qCHYnPlz2IokkLenDtkDBZG54AJ2Y4ezLOKPDHQaun5sF-Ml7X9-xRxiD59SMiXDxg__",
+    title: "Is Bandhan banking too much on microfinance?",
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    commentsCount: 23,
+    imageHeight: 140,
+    imageWidth: 250,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/dc14/c168/a437cbcac0ff201ec24f2c85a9f04d55?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cJ-l9FeoZtPdqqlpvK5SsaMJsk1WPCLpub8cT4yK1liHelP~KwomB0-MAvWYU-Fy7EE08eXCBMery6odqRrp8YLwp2JcBgqufCUjAT~o8a0EHuQw~-pgslExZQ7qYsSZxoT5Dl74zPNusS1rVzmrgChxeN9mrByYxtHhttilyxre7AQOSSLwJMb26fnDmwqAY7npWkNogIqds-bIHQdPJ8fM22En-0aYJ1vIpGihzQIAKHFMWeLZ55ml~v5QvrBVZcrPKGmYLyBHtSfMB-d2NAZdlccL0bl~uKKPyvLgg2zJvpQsocMn3Am3gDiXitrY~OO7POSpBcLLJ6v1ilvJGA__",
+    title: "Is Bandhan banking too much on microfinance?",
+
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    commentsCount: 23,
+    imageHeight: 140,
+    imageWidth: 250,
+  },
+  {
+    newsCategory: "Business",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/2f8d/35e9/ae7eee1b1d07e829b629c397cb4d2849?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Xns~1vEH-MT9bxydnTJLzvp8cdmXs1Fgh6UdJknvbg-bQrIAdjpetVG~aYXiRZMnVg8150fmRnM4Q~5EqK7hJa19j3TbZiLT3DgBEUKM6FzS1yJSrVnuiuhR3Ne-Z7619gEoaQq2ngo22u4LZ0AXI7bdrjY8gMEaHijyGNDCgpkz23Fx3fIcRx0pGQ67nX9IjiKI~GUfVvcuVhgMM5t6viROUXgjZJATyAnaC~j~l77CtzzbWKNfMsaSIpda7D1tkU5VbPbp-OmXgpmTmCjyW~tM1~qjJHkKj2WcKjKTdA33xZkfQtbeetOHx~KmatMhd8p6OYJ-n-qTAtpDPjq7Ng__",
+    title: "Is Bandhan banking too much on microfinance?",
+
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+
+    commentsCount: 23,
+    imageHeight: 140,
+    imageWidth: 250,
+  },
+];
+
+export const latestNewsLetterArray = [
+  {
+    newsCategory: "street smart",
+    newsImage:
+      "https://s3-alpha-sig.figma.com/img/460b/7ba2/cef7f3b55ea636ab34295402fb4c9c30?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a41EfstAmc9klHyhl1MA9SX0rPfpMnXG7cXyqEXy00KTKcpj5oT1b1-Wri-3wQEC8oH1nVpG7xhEiWwfn1YQ5IMURA6QGna5yb-SzJ1jfKGMxINgFq~riFmaolWVICAvERDK~PxSNrvG0rLXIwNDUpg4YqTQdNj4QjWdZHbHM9KoFXxGEyFkfoMbcZ9En4wMudeHR0gH6bMqo020BH19fiI~Ix9ngcyix6HJZqtuwhAHtTG9BPdiuzNdbI2qBVfcGgBvR~53oLWYk43zqHt3qCHYnPlz2IokkLenDtkDBZG54AJ2Y4ezLOKPDHQaun5sF-Ml7X9-xRxiD59SMiXDxg__",
+    title: "Is Bandhan banking too much on microfinance?",
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    commentsCount: 23,
+    imageHeight: 200,
+    imageWidth: 300,
+    articleReadTime: 5,
+  },
+  {
+    newsCategory: "street smart",
+    title: "Johnny Depp’s doctor, nurse testify; actor expected to take stand?",
+
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "street smart",
+    title:
+      "A Democrat in gym shorts tries to rally blue votes in Trump country",
+
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "street smart",
+    title: "Johnny Depp’s doctor, nurse testify; actor expected to take stand",
+
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    commentsCount: 23,
+  },
+  {
+    newsCategory: "street smart",
+    title:
+      "A Democrat in gym shorts tries to rally blue votes in Trump country",
+
+    authorSrc:
+      "https://s3-alpha-sig.figma.com/img/165b/bcf9/a5d1449563b4d9c6d37cb48cbafefa5c?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QxDQuvwcG90cD-N-IHiHYKH5V8itEy2tvAvyZfQcMlrgA7IVc7NUHRyz9mCHASEsHwKzsb4oyGzaEXiQS0oMVFUsxzlNWjIzu8bOvbrk211g9WHSgBLq-eYutRjaab9R9xlnj5eT0dmq~7dXw~XaOtPveFDiR4dTDqAi6RagqRLZk4Ts-4GnuUwcbthc7smiRlCCRmaifgdDlmtz6yKxGQSmDeAo59Kut8IJQh-8CmqOxQ8cOIeV4p7O96yVOzGkOzpUHbsNnikX2fGJtJ55RyhS2ofArIPQBKZ4JFmAQ2q9bBqN2VsUuj~Gs6ue9dlZ0npHdT02h335UDCzud9iRQ__",
+    authorName: " Ashish K. Mishra",
+    commentsCount: 23,
+  },
+];
